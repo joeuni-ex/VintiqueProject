96779c4a4cc2ffffa10f2d3f9f27172c0fff8f82
joeuni <dmsdl8917@gmail.com>
2024-01-06 22:26:52 +0900

    홈페이지 메인컨텐츠 2까지 완성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import "./Home.css";
 import mainImg from "../../assets/main.jpg";
+import Product from "../../components/product/Product";
 
 const Home = () => {
+  const listCount = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <main>
       <div className="container">
@@ -18,6 +20,36 @@ const Home = () => {
           <div className="buyBtn">
             <p>BUY Now</p>
           </div>
+        </div>
+      </div>
+      <div className="container-list">
+        <div className="ListTitle">
+          <h2>Browse The Range</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="HomeMainContent2">
+          <div className="MainList">
+            <img src={mainImg} alt="" />
+            <p>Dining</p>
+          </div>
+          <div className="MainList">
+            <img src={mainImg} alt="" />
+            <p>Living</p>
+          </div>
+          <div className="MainList">
+            <img src={mainImg} alt="" />
+            <p>Bedroom</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-list">
+        <div className="ListTitle">
+          <h2>Our Products</h2>
+        </div>
+        <div className="HomeMainContent3">
+          {listCount.map((product, index) => (
+            <Product key={index} />
+          ))}
         </div>
       </div>
     </main>
