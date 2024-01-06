1f7a8c23db6b0fefe5d74dad1ec2d90fa05f04f2
joeuni <dmsdl8917@gmail.com>
2024-01-06 18:57:03 +0900

    네브바

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { BsCart } from "react-icons/bs";
+import { IoSearch } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa6";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="navbar">
+        <div style={{ marginLeft: "5%" }}>
+          <p>😻로고</p>
+        </div>
+        <div>
+          <ul className="navbar_link">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Shop</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">
+                <li>Contact</li>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div style={{ marginRight: "5%" }}>
+          <ul className="navbar_link">
+            <li>
+              <a href="">
+                <BsCart />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <IoSearch />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaRegHeart />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaRegUser />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
