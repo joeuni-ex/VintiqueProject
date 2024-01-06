a5a2965c678ddb4ce994fe8765c18c06b31bd803
joeuni <dmsdl8917@gmail.com>
2024-01-06 22:26:36 +0900

    네브바 경로 재설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
