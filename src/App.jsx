9f0ba432c40aa11172c4816d1da68b201af9cb16
joeuni <dmsdl8917@gmail.com>
2024-01-06 20:53:58 +0900

    라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       {/* 라우팅 */}
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
