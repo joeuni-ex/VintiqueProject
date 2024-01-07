1a369d24226e388b5d3093b818953e992d26734c
joeuni <dmsdl8917@gmail.com>
2024-01-07 13:17:07 +0900

    리듀서 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/configStore.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* 리듀서 적용 */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
