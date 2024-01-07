2aa6ecbc3bfbd49d35a251e4aaf1ddb5fe9c31db
joeuni <dmsdl8917@gmail.com>
2024-01-07 11:53:21 +0900

    가입하기 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
 import Footer from "./components/footer/Footer";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="home" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
