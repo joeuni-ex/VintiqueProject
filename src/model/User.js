7df19c2dc071531094e3d511946c02006c641fe9
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-08 09:31:24 +0900

    유저 클래스 address추가

--- src/model/User.js
+++ src/model/User.js
@@ -1,8 +1,9 @@
 export default class User {
-  constructor(username, password, name, role, token, id) {
+  constructor(username, password, name, address, role, token, id) {
     this.username = username;
     this.password = password;
     this.name = name;
+    this.address = address;
     this.role = role;
     this.token = token;
     this.id = id;
