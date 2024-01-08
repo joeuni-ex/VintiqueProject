387e3dd8d79f814ebdc5edd2b697402af05653cf
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-08 10:34:35 +0900

    password2 추가

--- src/model/User.js
+++ src/model/User.js
@@ -1,7 +1,8 @@
 export default class User {
-  constructor(username, password, name, address, role, token, id) {
+  constructor(username, password, password2, name, address, role, token, id) {
     this.username = username;
     this.password = password;
+    this.password2 = password2;
     this.name = name;
     this.address = address;
     this.role = role;
