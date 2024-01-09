9d8361b7ca51b527a0cd9eb98c988eb44f397d26
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-09 14:12:42 +0900

    admin 페이지 구분

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import Banner from "../../components/banner/Banner";
 import "./Admin.css";
 
 const Admin = () => {
+  const [selectCategory, setSelectCategory] = useState("주문 리스트");
+
+  const categoryChange = (title) => {
+    setSelectCategory(title);
+  };
+  const title = "AdminPage";
   return (
-    <div div className="shopContainer">
-      <Banner title="AdminPage" />
+    <div className="shopContainer">
+      <Banner title={title} />
+      <div className="userPage">
+        <div className="user-category">
+          <ul className="list">
+            <li onClick={() => categoryChange("주문 리스트")}>주문 리스트</li>
+            <li onClick={() => categoryChange("상품 등록하기")}>
+              상품 등록하기
+            </li>
+            <li onClick={() => categoryChange("권한 변경")}>권한 변경</li>
+          </ul>
+        </div>
+        <div className="user-content">
+          <p>{selectCategory}</p>
+
+          <div className="test">
+            <table></table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
