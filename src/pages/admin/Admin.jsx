e11d649e6163409d7a85c1da03d8138c613e4ef6
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-09 12:21:18 +0900

    유저페이지 및 관리자 페이지 컴포넌트

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./Admin.css";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return <div>관리자 페이지</div>;
 };
 
 export default Admin;
---
+++ src/pages/mypage/MyPage.jsx
@@ -0,0 +1,6 @@
+import "./MyPage.css";
+const MyPage = () => {
+  return <div>MyPage</div>;
+};
+
+export default MyPage;
