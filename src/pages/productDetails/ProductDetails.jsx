1a93d89bd6b563008ce7495f87fb370ee6d04b3b
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-09 12:21:53 +0900

    상품 상세보기 페이지

--- src/pages/productDetails/ProductDetails.jsx
+++ src/pages/productDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import "./ProductDetails.css";
 
 const ProductDetails = () => {
-  return <div>디테일 페이지 입니다. </div>;
+  return <div className="shopContainer">상품 디테일 페이지 입니다. </div>;
 };
 
 export default ProductDetails;
