f93c68c74b7d86a7e15abfdedf9fdba183843d27
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-09 14:12:36 +0900

    마이페이지 오류 수정

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import Banner from "../../components/banner/Banner";
 import "./MyPage.css";
 const MyPage = () => {
+  const title = "MyPage";
+
   return (
-    <div div className="shopContainer">
-      <Banner title="MyPage" />
+    <div className="shopContainer">
+      <Banner title={title} />
     </div>
   );
 };
