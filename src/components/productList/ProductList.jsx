dad8ba0f3dd4997afb9f2088ac9c7aa5eb819875
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-10 12:21:45 +0900

    제품 관리 테이블 추가

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <tr>
+      <td>James</td>
+      <td>Matman</td>
+      <td>(713) 123-8965</td>
+      <td>
+        <a href="mailto:jmatman@stewart.com">jmatman@stewart.com</a>
+      </td>
+      <td>01/13/1979</td>
+    </tr>
+  );
+};
+
+export default ProductList;
--- src/pages/admin/AdminProduct.jsx
+++ src/pages/admin/AdminProduct.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AdminCate from "../../components/userCategory/AdminCate";
+import ProductList from "../../components/productList/ProductList";
 
 // TODO 제품 관리 페이지 -> GET Product
 const AdminProduct = () => {
@@ -10,6 +11,30 @@ const AdminProduct = () => {
         <AdminCate select="2" />
         <div className="user-content">
           <p>제품 관리</p>
+          <table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Date of Birth</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
