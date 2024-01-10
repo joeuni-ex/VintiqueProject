318b820d5cf7cf17a9b0bd08bf20095cc33028f5
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-10 11:04:21 +0900

    관리자페이지 카테고리 스타일 변경

--- src/components/userCategory/AdminCate.jsx
+++ src/components/userCategory/AdminCate.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import "./AdminCate.css";
 
 const AdminCate = () => {
   return (
     <div className="user-category">
+      <p>관리자페이지</p>
       <ul className="list">
         <Link to="/admin/order">
           <li>주문 관리</li>
