f4f5cadc1aa5a9673da9b4bfb3ba787ebd2e4b00
joeuni <dmsdl8917@gmail.com>
2024-01-11 14:17:45 +0900

    제품 클래스에 메인이미지 추가

--- src/model/Product.js
+++ src/model/Product.js
@@ -1,10 +1,20 @@
 export default class Product {
-  constructor(name, description, category, price, stock, boardImageList) {
+  constructor(
+    name,
+    description,
+    category,
+    price,
+    stock,
+    mainImage,
+    boardImageList,
+    id
+  ) {
     this.name = name;
     this.description = description;
     this.category = category;
     this.price = price;
     this.stock = stock;
+    this.mainImage = mainImage;
     this.boardImageList = boardImageList;
     this.id = id;
   }
