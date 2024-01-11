400013b131a048d49ce7a6f7588d821eee9a55c5
joeuni <dmsdl8917@gmail.com>
2024-01-11 09:29:35 +0900

    제품 서비스, 저장, 삭제, 조회 추가

---
+++ src/services/product.service.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { BASE_API_URL } from "../common/constants";
+import { authHeader } from "./base.service";
+
+const API_URL = BASE_API_URL + "/api/products";
+
+class ProductService {
+  //제품 저장
+  saveProduct(product) {
+    return axios.post(API_URL, product, { headers: authHeader() }); //인증이 필요하여 헤더에 토큰 들어감
+  }
+  //제품 삭제
+  deleteProduct(product) {
+    return axios.delete(API_URL + "/" + product.id, { headers: authHeader() }); //인증이 필요하여 헤더에 토큰 들어감
+  }
+  //모든 제품 조회
+  getAllProducts() {
+    return axios.get(API_URL); //조회는 인증이 필요없어서 headers 안들어감
+  }
+}
+//객체로 만들어서 사용(export)
+const productService = new ProductService();
+
+export default productService;
