3229ccb7e5f99d08da30fc9a99af76207aab3c37
joeuni <dmsdl8917@gmail.com>
2024-01-11 09:29:08 +0900

    제품 모델

---
+++ src/model/Product.js
@@ -0,0 +1,11 @@
+export default class Product {
+  constructor(name, description, category, price, stock, boardImageList) {
+    this.name = name;
+    this.description = description;
+    this.category = category;
+    this.price = price;
+    this.stock = stock;
+    this.boardImageList = boardImageList;
+    this.id = id;
+  }
+}
