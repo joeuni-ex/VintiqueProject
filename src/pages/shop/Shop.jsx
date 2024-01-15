23b76d8379f587f2441a85121a8fdc3461578517
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-15 14:50:43 +0900

    정렬 스타일 수정

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -7,10 +7,9 @@ import Pagination from "../../components/pagination/Pagination";
 import BottomBanner from "../../components/banner/BottomBanner";
 
 const Shop = () => {
-  const maxPageSize = 12;
-
-  const [page, setPage] = useState(0);
-  const [totalPage, setTotalPage] = useState(0);
+  const [maxPageSize, setMaxPageSize] = useState(12); //한 페이지에 최대 출력할 제품 개수
+  const [page, setPage] = useState(0); // 현재 페이지
+  const [totalPage, setTotalPage] = useState(0); //총 페이지
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
@@ -20,12 +19,49 @@ const Shop = () => {
       setTotalPage(response.data.pageable.pageSize); //총 페이지
       //console.log(response.data);
     });
-  }, [page]);
+  }, [page, maxPageSize]);
+
+  const handleChangeMaxShow = (e) => {
+    setMaxPageSize(e.target.value);
+  };
 
   return (
     <div className="shopContainer">
       <Banner title="Shop" subTitle="Shop" category="전체" />
-      <div className="sort">정렬</div>
+      <div className="shop-option-container">
+        <div
+          style={{
+            width: "1200px",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="shop-option-left">
+            <div>▶Dining</div>
+            <div>▶Living</div>
+            <div>▶Bedroom</div>
+          </div>
+          <div className="shop-option-rigth">
+            <div>
+              Show
+              <input
+                onChange={handleChangeMaxShow}
+                type="text"
+                className="shop-option-rigth-show"
+                placeholder={maxPageSize}
+              />
+            </div>
+            <div style={{ marginLeft: "30px" }}>
+              Short by
+              <select name="" id="" className="shop-option-rigth-sort">
+                <option value="1">Default</option>
+                <option value="2">가격낮은순</option>
+                <option value="3">가격높은순</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container-list">
         <div className="HomeMainContent3">
           {productList.map((product, index) => (
