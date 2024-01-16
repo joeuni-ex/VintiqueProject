2e2fb296a7ca1faab3545162187718cc02c744a0
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-16 16:28:43 +0900

    리뷰 컴포넌트 스타일 수정

--- src/components/productDetail/Reviews.jsx
+++ src/components/productDetail/Reviews.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import Star from "../rate/Star";
+import "./Reviews.css";
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  return (
+    <div className="basic-container">
+      <div className="review-rate-content">
+        <Star width={30} height={30} />
+        <p className="review-rate">4.5</p>
+        <p>
+          <span style={{ fontWeight: "bold" }}>5명</span>의 고객님이 리뷰를
+          남겨주셨습니다.
+        </p>
+      </div>
+      <div className="reivew-content">
+        <div style={{ display: "flex" }}>
+          <div>
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+          </div>
+          <div style={{ display: "flex" }}>
+            <p>홍길동</p>
+            <p>2022-09-11</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et odio
+            asperiores illo a, harum, ab libero consequatur architecto fugiat
+            inventore molestiae suscipit rerum possimus quos ut officiis
+            voluptatem accusantium beatae.
+          </p>
+        </div>
+      </div>
+      <div className="reivew-content">
+        <div style={{ display: "flex" }}>
+          <div>
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+          </div>
+          <div style={{ display: "flex" }}>
+            <p>홍길동</p>
+            <p>2022-09-11</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et odio
+            asperiores illo a, harum, ab libero consequatur architecto fugiat
+            inventore molestiae suscipit rerum possimus quos ut officiis
+            voluptatem accusantium beatae.
+          </p>
+        </div>
+      </div>
+      <div className="reivew-content">
+        <div style={{ display: "flex" }}>
+          <div>
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+            <Star width={20} height={20} />
+          </div>
+          <div style={{ display: "flex" }}>
+            <p>홍길동</p>
+            <p>2022-09-11</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et odio
+            asperiores illo a, harum, ab libero consequatur architecto fugiat
+            inventore molestiae suscipit rerum possimus quos ut officiis
+            voluptatem accusantium beatae.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
