dcfcd6c06eab6dca28a089767f2e042581cb95a9
joeuni <dmsdl8917@gmail.com>
2024-01-18 21:39:23 +0900

    마이페이지 스타일 수정중

---
+++ src/components/userCategory/MyPageCate.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const MyPageCate = ({ select }) => {
+  const [selected, setSelected] = useState("");
+  useEffect(() => {
+    setSelected(select);
+  }, [select]);
+
+  return (
+    <div className="user-category ">
+      <p>마이페이지</p>
+      <ul className="list">
+        <Link
+          className={selected === "1" ? "selected" : "list-link"}
+          to="/user/order"
+        >
+          <li>주문 관리</li>
+        </Link>
+        <Link
+          className={selected === "2" ? "selected" : "list-link"}
+          to="/role-change"
+        >
+          <li>권한 관리</li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default MyPageCate;
--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,10 +1,45 @@
 import { useEffect, useState } from "react";
 import Banner from "../../components/banner/Banner";
 import "./MyPage.css";
-const MyPage = () => {
-  // const title = "MyPage";
 
-  return <div className="shopContainer">{/* <Banner title={title} /> */}</div>;
+import MyPageCate from "../../components/userCategory/MyPageCate";
+import OrderList from "../../components/orderList/OrderList";
+
+const MyPage = () => {
+  const title = "MyPage";
+
+  return (
+    <div className="basic-container">
+      {/* 배너 */}
+      {<Banner title={title} />}
+      <div className="userPage">
+        <MyPageCate select="1" />
+        <div className="user-content">
+          {/* <p>주문 관리</p> */}
+          <div
+            style={{ display: "flex", width: "100%", justifyContent: "end" }}
+          ></div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>고객번호</th>
+                <th>고객명</th>
+                <th>구매제품</th>
+                <th>가격</th>
+                <th>주문일자</th>
+                <th>배송상태</th>
+              </tr>
+            </thead>
+            <tbody>
+              <OrderList />
+            </tbody>
+          </table>
+          {/* / <Pagination page={page} setPage={setPage} totalPage={totalPage} /> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
