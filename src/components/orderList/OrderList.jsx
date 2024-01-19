baff4a0f0206e87604d7cddea3707369ffa1a3ec
joeuni <dmsdl8917@gmail.com>
2024-01-19 15:58:48 +0900

    주문 상태 변경 및 주문 상세

--- src/components/orderList/OrderList.jsx
+++ src/components/orderList/OrderList.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import orderService from "../../services/order.service";
+import "./OrderList.css";
 
-const OrderList = ({ order }) => {
+const OrderList = ({ order, onStatusChange, onDetail }) => {
+  const [selected, setSelected] = useState(order?.status);
+
+  //주문 상태 변경
+  const handleChangeStatus = (e) => {
+    setSelected({ status: e.target.value });
+  };
+
+  const saveStatus = async () => {
+    if (confirm("주문상태를 변경하시겠습니까?")) {
+      try {
+        await orderService.changeOrderStatus(order.id, selected).then(() => {
+          alert("정상적으로 변경되었습니다.");
+          onStatusChange(order.id, selected);
+        });
+      } catch (err) {
+        alert("주문 상태 변경 시 오류 발생");
+      }
+    }
+  };
   return (
     <tr>
       <td>{order?.userId}</td>
       <td>{order?.userName}</td>
       <td>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div>
-            <Link className="LinkTag" to={`/product/${order?.productId}`}>
-              {order?.name}
-            </Link>
-          </div>
+        <div onClick={onDetail} className="orderViewBtn">
+          주문상세
         </div>
       </td>
-
       <td>{order?.totalPrice.toLocaleString("ko-KR")} 원</td>
 
       <td>{order?.createDate}</td>
-
-      <td>{order?.status}</td>
-
       <td>
-        <a className="LinkTag" style={{ cursor: "pointer" }}>
-          삭제
-        </a>
-        /
+        <select
+          style={{ width: "100%", height: "25px" }}
+          onChange={handleChangeStatus}
+          defaultValue={selected}
+          name="status"
+        >
+          <option value="주문 완료">주문 완료</option>
+          <option value="배송중">배송중</option>
+          <option value=" 배송 완료">배송 완료</option>
+        </select>
+      </td>
+      <td onClick={saveStatus} style={{ cursor: "pointer" }}>
+        변경
       </td>
     </tr>
   );
--- src/pages/admin/AdminOrder.jsx
+++ src/pages/admin/AdminOrder.jsx
@@ -2,28 +2,53 @@ import { Link } from "react-router-dom";
 import ProductList from "../../components/productList/ProductList";
 import AdminCate from "../../components/userCategory/AdminCate";
 import "./AdminPage.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import OrderList from "../../components/orderList/OrderList";
 import Pagination from "../../components/pagination/Pagination";
 import orderService from "../../services/order.service";
+import OrderDetail from "../../components/orderList/OrderDetail";
 
 // TODO 주문 관리 페이지 -> GET Purchase
 const AdminOrder = () => {
   const [page, setPage] = useState(0);
   const [totalPage, setTotalPage] = useState(0);
   const [orderList, setOrderList] = useState([]);
+  const [selectedOrderIndex, setSelectedOrderIndex] = useState(null); //주문 상세 인덱스
+  const [orderDetails, setOrderDetails] = useState([]);
 
-  const maxPageSize = 5; //한 페이지에 출력할 게시물 개수
+  const maxPageSize = 12; //한 페이지에 출력할 게시물 개수
 
   useEffect(() => {
-    orderService.getAllOrder(page, maxPageSize).then((response) => {
-      setOrderList(response.data.content);
-      setPage(response.data.pageable.pageNumber); // 현재 페이지
-      setTotalPage(response.data.pageable.pageSize); //총 페이지
-    });
+    const fetchData = async () => {
+      orderService.getAllOrder(page, maxPageSize).then((response) => {
+        const updatedOrderList = response.data.content.map((order) => {
+          return { ...order, key: order.id }; // add a unique key
+        });
+        setOrderList(updatedOrderList);
+        setPage(response.data.pageable.pageNumber); // 현재 페이지
+        setTotalPage(response.data.pageable.pageSize); //총 페이지
+      });
+    };
+    fetchData();
   }, [page]);
 
-  console.log(orderList);
+  //상태 변경 시 리랜더링
+  const handleStatusChange = (orderId, newStatus) => {
+    const updatedOrderList = orderList.map((order) =>
+      order.id === orderId ? { ...order, status: newStatus } : order
+    );
+    setOrderList(updatedOrderList);
+  };
+
+  // 주문 상세 보여주기
+  const toggleDetails = (index, orderId) => {
+    console.log(orderId);
+    setSelectedOrderIndex(index === selectedOrderIndex ? null : index);
+
+    orderService
+      .getMyOrderView(orderId)
+      .then((res) => setOrderDetails(res.data));
+  };
 
   return (
     <div className="basic-container base-color">
@@ -41,15 +66,32 @@ const AdminOrder = () => {
               <tr>
                 <th>고객번호</th>
                 <th>고객명</th>
-                <th>구매제품</th>
+                <th>주문정보</th>
                 <th>가격</th>
                 <th>주문일자</th>
-                <th>배송상태</th>
+                <th>주문상태</th>
+                <th>옵션</th>
               </tr>
             </thead>
             <tbody>
               {orderList?.map((order, idx) => (
-                <OrderList key={idx} order={order} idx={order.idx} />
+                <React.Fragment key={idx}>
+                  <OrderList
+                    order={order}
+                    idx={idx}
+                    onStatusChange={handleStatusChange}
+                    onDetail={() => toggleDetails(idx, order.id)}
+                  />
+                  {selectedOrderIndex === idx && (
+                    <tr>
+                      <td colSpan="7">
+                        {orderDetails?.map((item, itemIdx) => (
+                          <OrderDetail key={itemIdx} item={item} />
+                        ))}
+                      </td>
+                    </tr>
+                  )}
+                </React.Fragment>
               ))}
             </tbody>
           </table>
