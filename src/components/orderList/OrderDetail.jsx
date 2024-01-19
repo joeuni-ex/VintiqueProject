b3451bab95c9623ae60160409a857888692d114d
joeuni <dmsdl8917@gmail.com>
2024-01-19 15:58:21 +0900

    주문상세 추가

---
+++ src/components/orderList/OrderDetail.jsx
@@ -0,0 +1,36 @@
+import "./OrderDetail.css";
+const OrderDetail = ({ item }) => {
+  return (
+    <div className="order-detail">
+      <div style={{ flex: 0.2 }}>
+        <p style={{ fontSize: "0.9rem", fontWeight: 500 }}>
+          주문번호: {item?.id}
+        </p>
+      </div>
+
+      <div className="order-page-product-img">
+        <img src={item.mainImage} alt="" />
+      </div>
+      <div style={{ flex: 0.2 }}>
+        <p style={{ fontSize: "0.9rem", fontWeight: 500 }}> {item?.name}</p>
+      </div>
+      <div style={{ flex: 0.2 }}>
+        <p style={{ fontSize: "0.9rem", fontWeight: 500 }}>
+          제품가격 : {item?.price.toLocaleString("ko-KR")}원
+        </p>
+      </div>
+      <div style={{ flex: 0.2 }}>
+        <p style={{ fontSize: "0.9rem", fontWeight: 500 }}>
+          수량 : {item?.quantity}개
+        </p>
+      </div>
+      <div style={{ flex: 0.2 }}>
+        <p style={{ fontSize: "0.9rem", fontWeight: 500 }}>
+          합계 : {item?.price * item?.quantity}원
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
