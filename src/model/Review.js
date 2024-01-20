6b0fc0b366a21fd06290c5d1e291c208a18f0ab8
joeuni <dmsdl8917@gmail.com>
2024-01-20 13:28:20 +0900

    리뷰 모델 추가

---
+++ src/model/Review.js
@@ -0,0 +1,7 @@
+export default class Review {
+  constructor(reviewContent, rate, productId) {
+    this.reviewContent = reviewContent;
+    this.rate = rate;
+    this.productId = productId;
+  }
+}
