c12b80c7174c126f75b2bfc80252ad46d9b9c0d2
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-22 15:53:54 +0900

    401 ,404 페이지 완성

--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./Error.css";
+import { MdErrorOutline } from "react-icons/md";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="basic-container error-page">
+      <p className="error-name">
+        <MdErrorOutline />
+        Page not found
+      </p>
+      <p>존재하지 않는 주소를 입력하셨거나, </p>
+      <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다. </p>
+
+      <div className="basic-btn error-btn">
+        <Link to="/">메인으로</Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
--- src/pages/error/UnAuthorized.jsx
+++ src/pages/error/UnAuthorized.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Error.css";
+import { MdErrorOutline } from "react-icons/md";
 const UnAuthorized = () => {
-  return <div>UnAuthorized</div>;
+  return (
+    <div className="basic-container error-page">
+      <p className="error-name">
+        <MdErrorOutline />
+        Unauthorized
+      </p>
+      <p>접근 권한이 없는 페이지입니다.</p>
+
+      <div className="basic-btn error-btn">
+        <Link to="/">메인으로</Link>
+      </div>
+    </div>
+  );
 };
 
 export default UnAuthorized;
