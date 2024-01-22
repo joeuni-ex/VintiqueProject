20b7745e2e1e61f00c441c532088c69b7e0913a2
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-22 14:18:18 +0900

    유저 별 리뷰 조회

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -5,21 +5,23 @@ import { authHeader } from "./base.service";
 const API_URL = BASE_API_URL + "/api/review";
 
 class ReviewService {
-  //구매 저장
+  //리뷰 저장
   saveReview(review) {
     return axios.post(API_URL, review, { headers: authHeader() });
   }
 
-  //제품 별 구매 조회
+  //제품 별 리뷰 조회
   getReviewByProduct(productId) {
     return axios.get(API_URL + "/" + productId, {
       headers: authHeader(),
     });
   }
 
-  //유저 별 상세 조회
-  getReviewByUserId() {
-    return axios.get(API_URL, { headers: authHeader() });
+  //유저 별 리뷰 조회
+  getReviewByUserId(page, maxpage) {
+    return axios.get(API_URL + "?page=" + page + "&maxpage=" + maxpage, {
+      headers: authHeader(),
+    });
   }
 
   //리뷰 삭제
