ca13f67300c2fb4a61263e6f71bec159ad06e885
joeuni <dmsdl8917@gmail.com>
2024-01-23 22:41:36 +0900

    푸터 완성

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,22 +7,35 @@ const Footer = () => {
     <footer>
       <div className="footerTop">
         <div>
-          <p>
+          <p style={{ marginBottom: "10px" }}>
             <Link to="/" className="logo">
               <ImVine /> Vintique
             </Link>
           </p>
-          <p>Lorem ipsum, dolor sit amet consectetur</p>
+          <p> 지금까지 없었던 감성적인 공간 Vintique</p>
         </div>
         <div>
-          <p>Links</p>
-          <ul>
+          <p className="footer-cate">Links</p>
+          <ul className="footer-links">
             <li>Home</li>
             <li>Shop</li>
             <li>About</li>
           </ul>
         </div>
-        <div></div>
+        <div>
+          <p className="footer-cate">Help</p>
+          <ul className="footer-links">
+            <li>결제 관련</li>
+            <li>고객 문의</li>
+            <li>개인정보 정책</li>
+          </ul>
+        </div>
+        <div>
+          <p className="footer-cate"></p>
+          <ul className="footer-links">
+            <li></li>
+          </ul>
+        </div>
       </div>
       <div className="footerBottom">
         <p>2024 Vintique. All rights reverved</p>
