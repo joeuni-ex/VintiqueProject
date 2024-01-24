0f9257b48553cb51b089f96eb0baa9ca6c054652
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2024-01-24 10:45:07 +0900

    about페이지

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,56 +1,71 @@
 import Banner from "../../components/banner/Banner";
-import mainImg from "../../assets/main.jpg";
+import aboutImg from "../../assets/about.jpg";
 import "./About.css";
 const About = () => {
   const title = "About"; //배너 이름
   return (
     <div className="basic-container">
+      {/* 배너 */}
       {<Banner title={title} />}
-      <div>
-        <div className="about-content">
-          <div className="about-content-img">
-            <img src={mainImg} alt="aboutImg" />
+      <div className="about-container">
+        {/* 상단 */}
+        <div className="about-content-top">
+          <p className="about-content-top-title">
+            "Lorem ipsum dolor sit, amet consectetur"
+          </p>
+          <div className="short-divider"></div>
+          <div className="about-content-top-text">
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
           </div>
-          <div>
-            <h2>title</h2>
-          </div>
-          <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
-            architecto repellat quo id maiores, dolorem, necessitatibus maxime
-            explicabo officia error ut aliquid! Deserunt iure ex incidunt
-            laborum itaque, facilis optio!
+          <div className="about-content-top-text">
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
           </div>
         </div>
-      </div>
-      <div>
-        <div className="about-content">
-          <div className="about-content-img">
-            <img src={mainImg} alt="aboutImg" />
-          </div>
-          <div>
-            <h2>title</h2>
-          </div>
-          <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
-            architecto repellat quo id maiores, dolorem, necessitatibus maxime
-            explicabo officia error ut aliquid! Deserunt iure ex incidunt
-            laborum itaque, facilis optio!
-          </div>
+        {/* 이미지 영역 */}
+        <div className="about-content-img">
+          <img src={aboutImg} alt="aboutImg" />
         </div>
-      </div>
-      <div>
-        <div className="about-content">
-          <div className="about-content-img">
-            <img src={mainImg} alt="aboutImg" />
-          </div>
+        {/* 하단 */}
+        <div className="about-content-bottom">
+          <p className="about-content-bottom-title">title</p>
           <div>
-            <h2>title</h2>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
           </div>
+          <p className="about-content-bottom-title">title</p>
           <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
-            architecto repellat quo id maiores, dolorem, necessitatibus maxime
-            explicabo officia error ut aliquid! Deserunt iure ex incidunt
-            laborum itaque, facilis optio!
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
+            <p>
+              architecto repellat quo id maiores, dolorem, necessitatibus maxime
+            </p>
           </div>
         </div>
       </div>
