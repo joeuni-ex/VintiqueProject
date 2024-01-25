e3c21242501775409b6d2693664c46626b49f55e
joeuni <dmsdl8917@gmail.com>
2024-01-25 22:52:59 +0900

    제품 상세보기 수정

--- src/components/productDetail/Description.jsx
+++ src/components/productDetail/Description.jsx
@@ -5,17 +5,17 @@ const Description = ({ product }) => {
     <>
       <div style={{ width: "70%", color: "#9F9F9F" }}>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          voluptas commodi cumque debitis, enim, ex ipsa repellat mollitia iste
-          delectus illum accusamus blanditiis libero veniam distinctio dolore
-          reiciendis ducimus temporibus!
+          우리 인테리어 소품 샵은 감각적이고 현대적인 공간을 추구하는 분들을
+          위한 특별한 선물과 소품들을 모아놓은 곳입니다. 집이나 사무실, 카페 등
+          어디에서든 잘 어울리는 아름다운 소품으로 여러분의 공간을 더욱 아름답게
+          꾸며보세요."
         </p>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A, atque
-          soluta! Dolorum sit, harum quo perspiciatis beatae cumque. Debitis
-          tempora quidem voluptatem, quam nesciunt eligendi officia eos
-          praesentium nulla rerum.
+          우리의 소품은 오롯이 당신을 위해 고안되었습니다. 세심한 디테일과
+          창의적인 디자인이 어우러진 제품들은 단순한 소품 이상의 가치를
+          전합니다. 각 소품은 특별한 순간을 감동적으로 연출해주며, 고객 여러분께
+          특별한 경험을 선사합니다.
         </p>
       </div>
       <div>
